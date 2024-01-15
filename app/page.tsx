acc692293747f71d44b0b92908b0a77795edb54d
Charlie <charles.j.montoya@gmail.com>
2024-01-14 23:19:54 -0500

    added projects route

--- app/page.tsx
+++ app/page.tsx
@@ -6,40 +6,46 @@ export default function Home() {
         <main className="text-gray-700 bg-slate-100 px-6">
             {/* Hero Section */}
             <div className="flex flex-col justify-between items-center py-16 sm:flex-row sm:justify-between sm:items-center sm:px-16">
-                <div className="sm:w-2/3">
+                <div className="sm:w-2/3 sm:pr-8">
                     <h2 className="text-4xl">Charles Montoya</h2>
 
                     <div className="mb-4 sm:text-lg sm:mt-6">
-                        <p className="text-lg my-2 sm:text-xl">
+                        <p className="text-lg my-3 sm:text-xl">
                             Software Developer
                         </p>
                         <p className="sm:text-lg">New York, NY</p>
-                        <Link
-                            href="mailto:charles.j.montoya@gmail.com"
-                            target="_blank"
-                            className="text-sky-700"
-                        >
-                            Email
-                        </Link>{" "}
-                        |{" "}
-                        <Link
-                            href="/assets/pdf/resume.pdf"
-                            target="_blank"
-                            className="text-sky-700"
-                        >
-                            Resume
-                        </Link>{" "}
-                        |{" "}
-                        <a
-                            href="https://linkedin.com/in/charlesmontoya"
-                            target="_blank"
-                            className="text-sky-700"
-                        >
-                            LinkedIn
-                        </a>
+                        <div className="text-xs mt-4 sm:text-base">
+                            <span className="text-sky-700 ">
+                                charles.j.montoya@gmail.com
+                            </span>{" "}
+                            |{" "}
+                            <Link
+                                href="/assets/pdf/resume.pdf"
+                                target="_blank"
+                                className="text-sky-700"
+                            >
+                                Resume
+                            </Link>{" "}
+                            |{" "}
+                            <a
+                                href="https://linkedin.com/in/charlesmontoya"
+                                target="_blank"
+                                className="text-sky-700"
+                            >
+                                LinkedIn
+                            </a>
+                        </div>
                     </div>
 
-                    <div className="sm:mt-8">
+                    <div className="my-4 sm:text-lg">
+                        Previously a financial derivatives contract analyst and
+                        CFA charterholder for various investment management
+                        firms on Wall Street. I am now a contract software
+                        engineer at a digital marketing agency where I have
+                        built solutions for a number of small businesses.
+                    </div>
+
+                    <div className="mt-4 mb-10 sm:mt-6">
                         <p className="text-xl mb-2">Technologies:</p>
                         <p className="mb-2 sm:text-lg">
                             <span className="font-semibold">Frontend:</span>{" "}
@@ -60,13 +66,6 @@ export default function Home() {
                             Cloud
                         </p>
                     </div>
-                    <div className="text-justify my-4 sm:text-lg">
-                        Previously a financial derivatives contract analyst and
-                        CFA charterholder for various investment management
-                        firms on Wall Street. I am now a contract software
-                        engineer at a digital marketing agency where I have
-                        built solutions for a number of small businesses.
-                    </div>
                 </div>
                 <div>
                     <Image
@@ -85,7 +84,7 @@ export default function Home() {
                 <div className="mb-6">
                     <div className="flex flex-col sm:flex-row">
                         <p className="mb-2 text-sky-700 sm:text-lg sm:w-2/3">
-                            Built Customer Loyalty Program for Restaurant
+                            Customer Loyalty Program for Restaurant
                         </p>
                         <div>
                             <p className="mb-2 text-justify sm:ml-2 sm:text-lg">
@@ -103,11 +102,10 @@ export default function Home() {
                                     NextJS, Firebase, MailSend, Twilio
                                 </p>
                                 <Link
-                                    href="https://youtu.be/dSTr10kPeYY"
-                                    target="_blank"
+                                    href="/projects/reward-club"
                                     className="text-red-700"
                                 >
-                                    video
+                                    Demo
                                 </Link>
                             </div>
                         </div>
@@ -117,7 +115,7 @@ export default function Home() {
                 <div className="mb-6">
                     <div className="flex flex-col sm:flex-row">
                         <p className="mb-2 text-sky-700 sm:text-lg sm:w-2/3">
-                            Built Web-to-SMS Contact Form for Roofer
+                            Web-to-SMS Contact Form for Roofer
                         </p>
                         <div>
                             <p className="mb-2 sm:ml-2 sm:text-lg">
@@ -135,11 +133,10 @@ export default function Home() {
                                     React, Firebase, Twilio
                                 </p>
                                 <Link
-                                    href="https://youtu.be/Qkl-QvjrXhI"
-                                    target="_blank"
+                                    href="/projects/web-to-sms"
                                     className="text-red-700"
                                 >
-                                    video
+                                    Demo
                                 </Link>
                             </div>
                         </div>
@@ -149,7 +146,7 @@ export default function Home() {
                 <div className="mb-6">
                     <div className="flex flex-col sm:flex-row">
                         <p className="mb-2 text-sky-700 sm:text-lg sm:w-2/3">
-                            Built SMS Marketing Program for Jewelry Store
+                            SMS Marketing Program for Jewelry Store
                         </p>
                         <div>
                             <p className="mb-2 sm:ml-6 sm:text-lg">
@@ -167,11 +164,10 @@ export default function Home() {
                                     PHP, NodeJS, Twilio
                                 </p>
                                 <Link
-                                    href="https://youtu.be/K9ntyTDBULQ"
-                                    target="_blank"
+                                    href="/projects/sms-marketing"
                                     className="text-red-700"
                                 >
-                                    video
+                                    Demo
                                 </Link>
                             </div>
                         </div>
---
+++ app/projects/[id]/page.tsx
@@ -0,0 +1,140 @@
+import Link from "next/link";
+import React from "react";
+
+interface ProjectData {
+    id: string;
+    title: string;
+    desc: string;
+    highlights: string[];
+    stack: {
+        front: string[];
+        back: string[];
+    };
+    youtubeUrl: string;
+}
+
+const projectData = [
+    {
+        id: "reward-club",
+        title: "Reward Club",
+        desc: "A customer loyalty program that I built for a chicken wing restaurant in Queens, NY. They have a location in Astoria and another in Rego Park. The system is a web app served through the broswer on a table in the restaurants. Customers can signup via a tablet with mobile number or their email.  After the 4th visit, they earn a discount on their purchase. Customers and staff can also use a look-up function in order to check stats like current points and last time the customer checked in or earned a discount.",
+        highlights: [
+            "Over 4,000 users",
+            "Custom Solution to allow Mobile CheckIn",
+        ],
+        stack: {
+            front: ["NextJS", "CSS"],
+            back: ["Firebase", "Cloud Functions", "SendMail"],
+        },
+        youtubeUrl: "https://youtube.com/embed/dSTr10kPeYY",
+    },
+    {
+        id: "web-to-sms",
+        title: "Web to SMS",
+        desc: "A Web-to-SMS contact form I build for a roofer in Ansonia, CT. The roofer had previously made casual comments about how much spam they were getting to their contact forms and how it would result in them missing requests for estimates. This form is an alternative to standard email forms in that the potential client sends this message and the owner of the roofing company recieves the message directly to his mobile phone.  Since its deployment, this tool has recorded over 201 incoming requests for estimates, volume that the owner states is much more than what would previously come in from emails.",
+        highlights: [
+            "Elimiated Email Contact Form",
+            "Improved Sales Conversion",
+        ],
+        stack: {
+            front: ["NextJS", "CSS"],
+            back: ["Firebase", "Cloud Functions", "Twilio"],
+        },
+        youtubeUrl: "https://youtube.com/embed/Qkl-QvjrXhI",
+    },
+    {
+        id: "sms-marketing",
+        title: "SMS Marketing",
+        desc: "An SMS marketing service I built for a jewelry store in Yonkers, NY. One of my first independat projects, which I coded in PHP and delivered on a tablet which was on display in the jewelry store.  Over 6 years, we've helped the business collect over 8,000 mobile numbers, which they send promotional messages to throughout the year.",
+        highlights: ["Captured over 8,000", "include media files"],
+        stack: {
+            front: ["NextJS", "CSS"],
+            back: ["Firebase", "Cloud Functions", "Twilio"],
+        },
+        youtubeUrl: "https://youtube.com/embed/K9ntyTDBULQ",
+    },
+];
+
+// function to create slugs
+export async function generateStaticParams() {
+    // Return a list of possible value for id
+    return projectData.map((project) => {
+        return {
+            id: project.id,
+        };
+    });
+}
+
+function getPost({ params }: { params: { id: string } }) {
+    const project = projectData.find((project) => project.id === params.id);
+    return project;
+}
+
+const Project = ({ params }: { params: { id: string } }) => {
+    const project = getPost({ params });
+    console.log("project: ", project);
+    return (
+        <div className="flex flex-col items-center justify-center text-gray-700 bg-slate-100 p-6">
+            {/* Breadcrumbs */}
+            <div className="flex flex-col items-center justify-center bg-gray-50 sm:w-[760px] sm:border sm:border-gray-400 sm:rounded-lg sm:px-6 sm:shadow-xl">
+                <div className="w-full flex-start text-sm sm:text-base sm:pt-4">
+                    <Link href="/">home</Link>/{project?.id}
+                </div>
+                {/* Header */}
+                <div className="my-12">
+                    <h2 className="text-4xl font-semibold">{project?.title}</h2>
+                </div>
+                {/* Video */}
+                <div className="w-full">
+                    <iframe
+                        className="rounded-lg w-full aspect-video"
+                        src={project?.youtubeUrl}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+                {/* Footer */}
+                <div className="py-6 w-full items:start">
+                    <h4 className="sm:text-lg">
+                        <span className="font-semibold">Description: </span>
+                        {project?.desc}
+                    </h4>
+                    <div className="sm:text-lg">
+                        <p className="my-6">Highlights:</p>
+                        <ul className="list-disc px-4">
+                            {project?.highlights.map((highlight, index) => (
+                                <li key={index}>{highlight}</li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className="mt-6 sm:text-lg">
+                        <p className="my-4">Technologies:</p>
+                        {/* Front end Technologies */}
+                        <div className="mb-4 ml-4">
+                            <label htmlFor="front-tech">Front:</label>
+                            <ul id="front-tech" className="list-disc px-4">
+                                {project?.stack.front.map((tech, index) => (
+                                    <li key={index}>{tech}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        {/* Back end Technologies */}
+                        <div className="ml-4">
+                            <label htmlFor="back-tech">Back:</label>
+                            <ul id="back-tech" className="list-disc px-4">
+                                {project?.stack.back.map((tech, index) => (
+                                    <li key={index}>{tech}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
